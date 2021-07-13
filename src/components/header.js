8bcc41856e041b83815d0aedc5b9a5b2dd65129c
HayashiInShiga <67238088+HayashiInShiga@users.noreply.github.com>
2021-07-14 01:15:48 +0900

    add title art and fix layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,41 @@
 import { Link } from "gatsby";
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Header = ({ currentPage }) => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        width: "100%",
-        height: "10%",
-        left: "0",
-        top: "0",
-        background: "#272727",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-end",
-      }}
-    >
-      <Link to="/" style={{ color: "#ffffff", marginRight: "50px" }}>
-        Home
-      </Link>
-      <Link to="/profile" style={{ color: "#ffffff", marginRight: "50px" }}>
-        Profile
-      </Link>
-    </div>
+    <>
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "80px",
+          left: "0",
+          top: "0",
+          background: "#272727",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+        <StaticImage
+          src="../images/Title-Text.png"
+          alt="Title Image"
+          height={32}
+          style={{ marginLeft: "32px" }}
+        />
+        <Link
+          to="/"
+          style={{ color: "#ffffff", marginRight: "48px", marginLeft: "auto" }}
+        >
+          Home
+        </Link>
+        <Link to="/profile" style={{ color: "#ffffff", marginRight: "48px" }}>
+          Profile
+        </Link>
+      </div>
+      {/* <div style={{ width: "100%", height: "10%", left: "0", top: "0" }}></div> */}
+    </>
   );
 };
 
