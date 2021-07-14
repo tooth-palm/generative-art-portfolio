86ee2b21197cc7568dd3568b6f556a9b7ae0bd29
HayashiInShiga <67238088+HayashiInShiga@users.noreply.github.com>
2021-07-14 21:09:02 +0900

    make profile page

---
+++ src/components/profile-card.js
@@ -0,0 +1,51 @@
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
+import * as React from "react";
+
+const ProfileCard = () => {
+  return (
+    <>
+      <div
+        style={{
+          position: "relative",
+          width: "440px",
+          height: "480px",
+          background: "#333333",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          flexDirection: "column",
+          margin: "20px",
+          borderRadius: "8px",
+          boxShadow: "0px 0px 4px rgba(0, 0, 0, .5)",
+        }}
+      >
+        <StaticImage
+          src="../images/profile-image.png"
+          alt="Profile Image"
+          width={300}
+          style={{ marginTop: "20px", borderRadius: "8px" }}
+        />
+        <p style={{ color: "#CDCDCD", textAlign: "center" }}>
+          <b>Hayashizaki</b>
+          <br />
+          first year graduate student
+          <br />
+          at Kyoto Institue of Technology
+        </p>
+        <a
+          href="https://github.com/HayashiInShiga/generative-art-portfolio"
+          style={{
+            color: "#CDCDCD",
+            marginRight: "20px",
+            marginLeft: "auto",
+            marginBottom: "10px",
+          }}
+        >
+          View code on GitHub
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default ProfileCard;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,8 +1,30 @@
 import * as React from "react";
+import BackgroundImage from "../components/background";
 import Header from "../components/header";
+import ProfileCard from "../components/profile-card";
 
 const ProfilePage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <BackgroundImage />
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "0",
+          top: "10vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          zIndex: "-1",
+        }}
+      >
+        <ProfileCard />
+      </div>
+    </>
+  );
 };
 
 export default ProfilePage;
