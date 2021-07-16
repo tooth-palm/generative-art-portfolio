3fde7865379247b797f8c558004e980abc6d3e39
HayashiInShiga <67238088+HayashiInShiga@users.noreply.github.com>
2021-07-17 00:47:12 +0900

    スマホでのハンバーガーメニュー実装

--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,53 @@ import { Link } from "gatsby";
 import * as React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import "@fontsource/roboto";
-import Typography from "@material-ui/core/Typography";
+import { Drawer, Hidden, IconButton, Typography } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const Header = () => {
+  const [isDrawerOpen, setDrawerOpen] = React.useState(false);
+  const openDrawer = () => {
+    setDrawerOpen(true);
+  };
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  };
   return (
     <>
       <div style={headerWrapper}>
         <StaticImage
           src="../images/Title-Text.png"
           alt="Title Image"
-          height={32}
+          height={24}
           style={{ marginLeft: "32px" }}
         />
-        <Link to="/" style={homeButtonStyle}>
-          <Typography variant="button">Home</Typography>
-        </Link>
-        <Link to="/profile" style={profileButtonStyle}>
-          <Typography variant="button">Profile</Typography>
-        </Link>
+        <Hidden smUp>
+          <IconButton
+            color="inherit"
+            onClick={openDrawer}
+            style={homeButtonStyle}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer anchor="right" open={isDrawerOpen} onClose={closeDrawer}>
+            <div style={drawerBackground}>
+              <Link to="/" style={drawerButtonStyle}>
+                <Typography variant="h5">Home</Typography>
+              </Link>
+              <Link to="/profile" style={drawerButtonStyle}>
+                <Typography variant="h5">Profile</Typography>
+              </Link>
+            </div>
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown>
+          <Link to="/" style={homeButtonStyle}>
+            <Typography variant="button">Home</Typography>
+          </Link>
+          <Link to="/profile" style={profileButtonStyle}>
+            <Typography variant="button">Profile</Typography>
+          </Link>
+        </Hidden>
       </div>
     </>
   );
@@ -42,13 +71,26 @@ const headerWrapper = {
 
 const homeButtonStyle = {
   color: "#ffffff",
-  marginRight: "48px",
+  marginRight: "5%",
   marginLeft: "auto",
   textDecoration: "none",
 };
 
 const profileButtonStyle = {
   color: "#ffffff",
-  marginRight: "48px",
+  marginRight: "5%",
   textDecoration: "none",
+};
+
+const drawerBackground = {
+  height: "100vh",
+  width: "40vw",
+  background: "#CDCDCD",
+  textAlign: "center",
+};
+
+const drawerButtonStyle = {
+  margin: "20px",
+  textDecoration: "none",
+  color: "#272727",
 };
